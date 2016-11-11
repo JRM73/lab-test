69a3c990d081e6e8b31b81f0005bc6ab83c84624
malice <web@mlj-prod.ch>
2016-11-11 09:26:08 +0100

    Add text in About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,11 @@ var React = require('react');
 
 var About = (props) => {
   return(
+    <div>
     <h3>About Component</h3>
+      <p>Welcome on About page!</p>
+      <p>Yop!</p>
+    </div>
   );
 };
 
